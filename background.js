26d96636c707560f37fb7ec19404f5e3b66e273e
eoin.payne <eoinpayne@gmail.com>
2020-06-13 09:23:17 +0100

    Working, rough. Display word count in new window when tile clicked

---
+++ background.js
@@ -0,0 +1,12 @@
+
+window.word_count = {}  //window, because from popup.js we are accessing background window
+
+chrome.runtime.onMessage.addListener(function(request,sender,sendMessage){
+    window.word_count[request.url] = request.count     //add key:value url:count to track which site had what count
+
+})
+
+//we want a new window to open, with the above data inside
+chrome.browserAction.onClicked.addListener(function(tab){  //add listener will call callback with the tab which was clicked
+    chrome.tabs.create({url: "popup.html"})  //to open new tabs, need permissions set in manifest
+})
---
+++ content.js
@@ -0,0 +1,20 @@
+//content script now has access to all info passed in 
+//runs per page loaded if it matches url in manifest
+
+// //waits for signal from button in popup
+// chrome.runtime.onMessage.addListener(  //any time content.js gets called, like from popup.js, listener fires
+//     function(request, sender, sendResponse){      // request will be message sent //listener gives ability to send response, this is a function passed in from popup.js
+//         const re = RegExp ('how', "gi") //gi is case insensitive?
+//         const matches = document.documentElement.innerHTML.match(re)  //documentElement the entire docuement // find every instance of "how" on the page
+//         sendResponse({count : matches.length}) //response sent to where this was called(popup.js)
+        
+//         //alert(request)
+
+// })
+
+const re = RegExp ('how', "gi") 
+const matches = document.documentElement.innerHTML.match(re)
+chrome.runtime.sendMessage({
+    url: window.location.href, //the payload, where the count of "how" came from
+    count: matches.length
+})
